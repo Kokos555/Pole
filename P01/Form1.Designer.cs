ba38ef6ad8445ff2f2162ca5676223c2dad55af7
Vít Novotný <novotvit@hotmail.com>
2022-03-03 11:22:44 +0100

    P01 hotovo

--- P01/Form1.Designer.cs
+++ P01/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -43,7 +44,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Proveď";
+            this.button1.Text = "1 Metoda";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -96,11 +97,22 @@
             this.label3.Size = new System.Drawing.Size(0, 13);
             this.label3.TabIndex = 6;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(192, 213);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "2 Metoda";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1371, 764);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -124,6 +136,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- P01/Form1.cs
+++ P01/Form1.cs
@@ -19,17 +19,98 @@ namespace P01
 
         private void button1_Click(object sender, EventArgs e)
         {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
             Random value = new Random();
+            double max = 0;
+            double min = 1000;
+            int pozice_max = 0;
+            int pozice_min = 0;
+            double soucet = 0;
             int n = Convert.ToInt32(textBox1.Text);
+            int pocet = n;
             double[] pole = new double[n];
             for (int i = 0; i < n; i++)
             {
                 double cislo = value.NextDouble() * (999) + 1;
                 pole[i] = cislo;
             }
-            foreach(double prvek in pole)
+            foreach (double prvek in pole)
             {
-                listBox1.Items.Add(prvek.ToString());
+                listBox1.Items.Add(string.Format("{0:F2}", prvek));
+            }
+            for (int j = 0; j < n; j++)
+            {
+                if (max < pole[j])
+                {
+                    max = pole[j];
+                    pozice_max = j;
+                }
+                if (min > pole[j])
+                {
+                    min = pole[j];
+                    pozice_min = j;
+                }    
+                soucet += pole[j];
+            }
+            soucet -= max + min;
+            pocet -= 2;
+            if (pocet > 0)
+            {
+                double ar = soucet / pocet;
+                MessageBox.Show(string.Format("Aritmeticky prumer cisel v poli bez maxima a minima je {0:F2}", ar));
+            }
+            else
+            {
+                MessageBox.Show(string.Format("Nelze vypocitat aritmeticky prumer"));
+            }
+            pole[pozice_max] = min;
+            pole[pozice_min] = max;
+            for (int o = 0; o < n; o++)
+            {
+                listBox2.Items.Add(string.Format("{0:F2}", pole[o]));
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            Random value = new Random();
+            double soucet = 0;
+            int n = Convert.ToInt32(textBox1.Text);
+            int pocet = n;
+            double[] pole = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                double cislo = value.NextDouble() * (999) + 1;
+                pole[i] = cislo;
+                soucet += cislo;
+            }
+            foreach (double prvek in pole)
+            {
+                listBox1.Items.Add(string.Format("{0:F2}", prvek));
+            }
+            double max = pole.Max();
+            double min = pole.Min();
+            int pozice_max = Array.IndexOf(pole,max);
+            int pozice_min = Array.IndexOf(pole,min);
+            pole[pozice_max] = min;
+            pole[pozice_min] = max;
+            for (int o = 0; o < n; o++)
+            {
+                listBox2.Items.Add(string.Format("{0:F2}", pole[o]));
+            }
+            soucet -= max + min;
+            pocet -= 2;
+            if (pocet > 0)
+            {
+                double ar = soucet / pocet;
+                MessageBox.Show(string.Format("Aritmeticky prumer cisel v poli bez maxima a minima je {0:F2}", ar));
+            }
+            else
+            {
+                MessageBox.Show(string.Format("Nelze vypocitat aritmeticky prumer"));
             }
         }
     }
