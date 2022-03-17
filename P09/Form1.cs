cbfbca246121b59c17814b3ba48fc2b84c11eb00
Vít Novotný <novotvit@hotmail.com>
2022-03-17 11:32:14 +0100

    P09 skoro hotovo

---
+++ P09/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P09
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Random value = new Random();
+            int n = Convert.ToInt32(textBox1.Text);
+            int[] pole = new int[n];
+            for (int i = 0; i<n; i++)
+            {
+                int cislo = value.Next(1, 11);
+                pole[i] = cislo;
+            }
+            Array.Sort(pole);
+            int median = 0;
+            foreach (int cislo in pole)
+            {
+                listBox1.Items.Add(cislo);
+            }
+            if (n % 2 == 1)
+            {
+                int cislo = n / 2;
+                median = pole[cislo];
+
+            }else
+            {
+
+            }
+            label2.Text = string.Format("{0}", median);
+        }
+    }
+}
