f4c91a6d571c016f284a38b8d5daa4af99740e52
Vít Novotný <novotvit@hotmail.com>
2022-03-23 08:32:36 +0100

    P09 hotovo

--- P09/Form1.cs
+++ P09/Form1.cs
@@ -22,6 +22,8 @@ namespace P09
             listBox1.Items.Clear();
             Random value = new Random();
             int n = Convert.ToInt32(textBox1.Text);
+            int pocet = 0;
+            int cislo2 = 0;
             int[] pole = new int[n];
             for (int i = 0; i<n; i++)
             {
@@ -38,12 +40,32 @@ namespace P09
             {
                 int cislo = n / 2;
                 median = pole[cislo];
-
-            }else
-            {
-
+                for (int i = 0; i < n; i++)
+                {
+                    cislo2 = pole[i];
+                    if (cislo2 == median)
+                    {
+                        pocet++;
+                    }
+                }
+                label2.Text = string.Format("Median je cislo {0} a vyskytuje se tam {1}", median, pocet);
             }
-            label2.Text = string.Format("{0}", median);
+            else
+            {
+                int cislo = n / 2;
+                int median1 = pole[cislo];
+                int median2 = pole[cislo - 1];
+                double podil = ((double)median1 + median2)/2;
+                for (int i = 0; i < n; i++)
+                {
+                    cislo2 = pole[i];
+                    if (cislo2 == podil)
+                    {
+                        pocet++;
+                    }
+                }
+                label2.Text = string.Format("Median cisel je {0} a vyskytuje se tam {1}", podil,pocet);
+            }
         }
     }
 }
